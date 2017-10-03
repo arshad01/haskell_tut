a81bcf6160650b607795309ff40a2beb67de9986
Arshad Khan <khan.m.arshad@gmail.com>
2017-10-03 14:01:47 +0800

    ch1 ex

---
+++ ch1/hello.hs
@@ -0,0 +1,9 @@
+module Main where
+import System.Environment
+
+main :: IO()
+main = do --args <- getArgs
+          -- putStrLn ((args !! 0) ++ " + " ++ (args !! 1) ++ " = " ++ show(read(args !! 0) + read(args !! 1)))
+          putStrLn ("Please enter your name: ")
+          name <- getLine
+          putStrLn ("Hello, " ++ name)
